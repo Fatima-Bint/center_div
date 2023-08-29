afd51e12d82d30f7228e1b60ee7cc0f1450d84c7
Admin <fatimatib44@gmail.com>
2023-08-29 10:16:42 +0000

    portfolio updated

---
+++ arrayMtd.js
@@ -0,0 +1,11 @@
+const temperature = [
+    33, 12, 20, 16, 5, 25, 64, 32,
+    {name: "temp", patientId: 4, clinic: "Nyaho"},
+    {name: "temp", patientId: 5, clinic: "Trinity"},
+];
+
+const times = (a, b) => 
+    a*b  
+  
+
+  times()
